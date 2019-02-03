e2766e305846f5f3255b22590f3df9c1dc3a199e
NolanSun <shr1213@live.com>
2019-02-03 12:59:24 +0800

    fix bug

--- IpMonitor/ip_monitor.py
+++ IpMonitor/ip_monitor.py
@@ -64,10 +64,10 @@ def diff_extranet_ip(current_extranet_ip, current_intranet_ip):
 
     if (current_extranet_ip != history_extranet_ip or history_extranet_ip == None) and current_extranet_ip != None:
         global_var.set_value('current_extranet_ip', current_extranet_ip)
-        content = content + 'New extranet IP is ' + '[' + current_extranet_ip + ']' + '\n' + 'History extranet IP is ' + '[' + current_extranet_ip + ']' + '\r'
+        content = content + 'New extranet IP is ' + '[' + current_extranet_ip + ']' + '\n' + 'History extranet IP is ' + '[' + history_extranet_ip + ']' + '\r'
     if (current_intranet_ip != history_intranet_ip or history_intranet_ip == None) and current_intranet_ip != None:
         global_var.set_value('current_intranet_ip', current_intranet_ip)
-        content = content + 'New intranet IP is ' + '[' + current_intranet_ip + ']' + '\n' + 'History intranet IP is ' + '[' + current_intranet_ip + ']' + '\r'
+        content = content + 'New intranet IP is ' + '[' + current_intranet_ip + ']' + '\n' + 'History intranet IP is ' + '[' + history_intranet_ip + ']' + '\r'
     if content != '':
         ms = MailSender('IP Monitor', 'IP changed!', content)
         ms.send_it()
