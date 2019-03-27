71c032a81388df690eb4fb00ad0e6f2c1eff6efb
NolanSun <shr1213@live.com>
2019-03-27 18:47:13 +0800

    update

--- Common/model.py
+++ Common/model.py
@@ -19,22 +19,24 @@ class GoldPrice(peewee.Model):
     class Meta:
         database = database
 
+
 class App(peewee.Model):
     '''
         app_name, expect_price
     '''
-    app_name= peewee.CharField()
+    app_name = peewee.CharField()
     expect_price = peewee.IntegerField()
     is_valid = peewee.IntegerField()
 
     class Meta:
         database = database
 
+
 class AppPrice(peewee.Model):
     '''
         app name, price, date, crawling_times, time
     '''
-    app_name= peewee.CharField()
+    app_name = peewee.CharField()
     price = peewee.CharField()
     date = peewee.DateField()
     crawling_times = peewee.IntegerField()
@@ -43,6 +45,68 @@ class AppPrice(peewee.Model):
     class Meta:
         database = database
 
+
+class City(peewee.Model):
+    '''
+        city_name, province
+    '''
+    city_name = peewee.CharField()
+    province = peewee.CharField()
+
+    class Meta:
+        database = database
+
+
+class Province(peewee.Model):
+    '''
+        province_name
+    '''
+    province_name = peewee.CharField()
+
+    class Meta:
+        database = database
+
+
+class Weather(peewee.Model):
+    '''
+        city_code, max_temp, min_temp, date, time
+    '''
+    city_code = peewee.CharField()
+    max_temp = peewee.CharField()
+    min_temp = peewee.CharField()
+    date = peewee.DateField()
+    time = peewee.TimeField()
+
+    class Meta:
+        database = database
+
+
+class AQI(peewee.Model):
+    '''
+        city_code, aqi, main, pm10, pm25, no2, so2, co, o3
+    '''
+    city_code = peewee.CharField()
+    site_name=peewee.CharField()
+    aqi = peewee.CharField()
+    main = peewee.CharField()
+    pm10 = peewee.CharField()
+    pm25 = peewee.CharField()
+    no2 = peewee.CharField()
+    so2 = peewee.CharField()
+    co =peewee.CharField()
+    o3 = peewee.CharField()
+    date = peewee.DateField()
+    crawling_times = peewee.IntegerField()
+    time = peewee.TimeField()
+
+    class Meta:
+        database = database
+
+
 # GoldPrice().create_table()
 # AppPrice().create_table()
 # App().create_table()
+# Weather.drop_table()
+# Weather().create_table()
+# AQI().drop_table()
+# AQI().create_table()
---
+++ WeatherMonitor/weather_getter.py
@@ -0,0 +1,76 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import time
+import peewee
+import datetime
+import requests
+import platform
+import configparser
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+sys.path.append('../')
+sys.path.append('../../')
+from Common.Tools import Tools
+from Common.model import Weather, AQI, City
+
+
+def get_key():
+    cf = configparser.ConfigParser()
+    if 'Windows' in platform.platform() and 'Linux' not in platform.platform():
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Using C:/Users/sunhaoran/Documents/GitHub/ServerTools/ServerTools.config ...')
+        cf.read('C:/Users/sunhaoran/Documents/GitHub/ServerTools/ServerTools.config')
+    elif 'Linux' in platform.platform() and 'Ubuntu' not in platform.platform():
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Using /home/pi/Documents/Github/ServerTools/RaspberryPi.config ...')
+        cf.read('/home/pi/Documents/Github/RaspberryPi.config')
+    elif 'Ubuntu' in platform.platform():
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Using /root/Documents/GitHub/ServerTools/ServerTools.config ...')
+        cf.read('/root/Documents/GitHub/ServerTools/ServerTools.config')
+    key = (cf.get('config', 'KEY'))
+    return key
+
+
+def get_temp(key: str, location: str):
+    payload = {'location': location, 'key': key}
+    r = requests.get('https://free-api.heweather.com/s6/weather/forecast', params=payload)
+    today_forecast = r.json()['HeWeather6'][0]['daily_forecast'][0]
+    today_tmp_max = today_forecast['tmp_max']  # 今天最高气温
+    today_tmp_min = today_forecast['tmp_min']  # 今天最低气温
+    return (today_tmp_max, today_tmp_min)
+
+
+def get_aqi(key: str, location: str):
+    payload = {'location': location, 'key': key}
+    r = requests.get('https://free-api.heweather.com/s6/weather/forecast', params=payload)
+    r = requests.get('https://free-api.heweather.net/s6/air/now', params=payload)
+    return (r.json()['HeWeather6'][0])
+
+
+def save_temp(city_name, max_temp, min_temp, date, time):
+    city_code = City.select('id').where(City.city_name == city_name)
+    p = Weather(city_code=city_code, max_temp=max_temp, min_temp=min_temp, date=datetime.datetime.now().date(), time=datetime.datetime.now().strftime('%H:%M:%S'))
+    p.save()
+    print('data saved...')
+
+
+def save_aqi(city_name, site_name, aqi, main, pm10, pm25, no2, so2, co, o3):
+    city_code = AQI.select('id').where(City.city_name == city_name)
+    p = AQI(city_code=city_code, site_name=site_name, aqi=aqi, main=main, pm10=pm10, pm25=pm25, no2=no2, so2=so2, co=co, o3=o3, date=datetime.datetime.now().date(), time=datetime.datetime.now().strftime('%H:%M:%S'))
+    p.save()
+    print('data saved...')
+
+
+key = get_key()
+today_tmp_max, today_tmp_min = get_temp(key, 'changchun')
+
+aqi_json = get_aqi(key, 'changchun')
+city_aqi = aqi_json['air_now_city']
+city_aqi['site_name'] = '-'
+print(city_aqi)
+site_aqi=aqi_json['air_now_station']
+for single_aqi in site_aqi:
+    print('='*10)
+    print(single_aqi)
+    
